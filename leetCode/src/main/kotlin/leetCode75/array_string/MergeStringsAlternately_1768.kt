3087de9272e8a0714134764cf750a006967551c6
배만진 <gentlemmj@gmail.com>
2023-05-06 11:02:20 +0900

    add MergeStringsAlternately_1768

---
+++ leetCode/src/main/kotlin/leetCode75/array_string/MergeStringsAlternately_1768.kt
@@ -0,0 +1,64 @@
+package leetCode75.array_string
+
+fun main() {
+    val result = SolutionBest().mergeAlternately("abc", "pqr")
+    println(result)
+}
+
+// Time Complexity is O(max(n,m))
+class Solution1 {
+    fun mergeAlternately(word1: String, word2: String): String {
+        var result = ""
+        for (i in 0..Math.max(word1.length - 1, word2.length - 1)) {
+            if (i < word1.length) result += word1[i]
+            if (i < word2.length) result += word2[i]
+        }
+
+        return result
+    }
+}
+
+class Solution2 {
+    fun mergeAlternately(word1: String, word2: String): String {
+        var result = ""
+        var i = 0
+        while (i < word1.length || i < word2.length) {
+            if (i < word1.length) result += word1[i]
+            if (i < word2.length) result += word2[i]
+
+            i++
+        }
+
+        return result
+    }
+}
+
+class SolutionBest {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val result = StringBuilder()
+        var i = 0
+
+        while (i < word1.length || i < word2.length) {
+            if (i < word1.length) result.append(word1[i])
+            if (i < word2.length) result.append(word2[i])
+
+            i++
+        }
+
+        return result.toString()
+    }
+}
+
+class Solution4 {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val result = StringBuilder()
+        val maxIdx = Math.max(word1.length - 1, word2.length - 1)
+
+        for (i in 0..maxIdx) {
+            if (i < word1.length) result.append(word1[i])
+            if (i < word2.length) result.append(word2[i])
+        }
+
+        return result.toString()
+    }
+}
