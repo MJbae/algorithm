80801f183f6d4651c647ec33b0bb06e00bb40cc5
MJbae <111344711+mj-at-ogq@users.noreply.github.com>
2023-05-11 18:45:23 +0900

    add KidsWiththeGreatestNumberofCandies_1431

---
+++ leetCode/src/main/kotlin/leetCode75/array_string/KidsWiththeGreatestNumberofCandies_1431.kt
@@ -0,0 +1,30 @@
+package leetCode75.array_string
+
+fun main() {
+    val result = Solution3Best().kidsWithCandies(intArrayOf(12, 1, 12), 10)
+    println(result)
+}
+
+class Solution3_1 {
+    fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
+        val maxCandy = candies.max()
+        return candies.map { candy -> checker(candy as Int, maxCandy as Int, extraCandies as Int) }.toList()
+    }
+
+    private fun checker(candy: Int, maxCandy: Int, extraCandies: Int): Boolean {
+        if (candy == maxCandy) return true
+
+        val candidate = candy + extraCandies
+        if (candidate >= maxCandy) return true
+        return false
+    }
+}
+
+class Solution3Best {
+    fun kidsWithCandies(candies: IntArray, extraCandies: Int): List<Boolean> {
+        val max = candies.max() ?: 0
+        return candies.mapIndexed { _, value ->
+            value + extraCandies >= max
+        }
+    }
+}
