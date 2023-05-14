6e2dbae8bb841e176bca3cd8090676dd9567969e
MJbae <111344711+mj-at-ogq@users.noreply.github.com>
2023-05-14 16:02:27 +0900

    enhance readability of 345 better

--- leetCode/src/main/kotlin/leetCode75/array_string/ReverseVowelsofaString345.kt
+++ leetCode/src/main/kotlin/leetCode75/array_string/ReverseVowelsofaString345.kt
@@ -8,24 +8,34 @@ fun main() {
 
 class Solution5a {
     fun reverseVowels(s: String): String {
+        val result = StringBuilder(s)
+
+        val vowelsMap = extractVowels(s)
+
+        reverseVowels(result, vowelsMap)
+
+        return result.toString()
+    }
+
+    private fun extractVowels(s: String): MutableMap<Int, Char>{
         val vowels = listOf('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
-        val charMap: MutableMap<Int, Char> = mutableMapOf()
+        val vowelsMap: MutableMap<Int, Char> = mutableMapOf()
         val result = StringBuilder(s)
 
         for (index in result.indices){
             if(result[index] in vowels){
-                charMap[index] = result[index]
+                vowelsMap[index] = result[index]
             }
         }
 
-        val keys = charMap.keys.toList()
-        val valuesReverse = charMap.values.reversed()
-        val reversedChar = keys.zip(valuesReverse)
+        return vowelsMap
+    }
+    private fun reverseVowels(result: StringBuilder, vowelsMap: MutableMap<Int, Char>) {
+        val keys = vowelsMap.keys.toList()
+        val valuesReverse = vowelsMap.values.reversed()
 
-        for ((key, value) in reversedChar){
+        for ((key, value) in keys.zip(valuesReverse)){
             result[key] = value
         }
-
-        return result.toString()
     }
 }
