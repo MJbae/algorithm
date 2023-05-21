0f3cb09ed0c7164037fb12be015f2be624c4b693
배만진 <gentlemmj@gmail.com>
2023-05-21 11:36:14 +0900

    add IsSubsequence392 best solution

--- leetCode/src/main/kotlin/leetCode75/two_pointers/IsSubsequence392.kt
+++ leetCode/src/main/kotlin/leetCode75/two_pointers/IsSubsequence392.kt
@@ -29,26 +29,17 @@ class Solution2a {
     }
 }
 
-// space complexity: , time complexity:
-class Solution2b {
+// space complexity: O(1), time complexity: O(n)
+class Solution2Best {
     fun isSubsequence(s: String, t: String): Boolean {
-        // t의 요소를 앞에서부터 제거하면서 인덱스 저장
-        var lastCharAt = 0
-        var matchCount = 0
-        s.forEach outer@{ sChar ->
-            t.forEachIndexed inner@{ index, c ->
-                if (sChar == c) {
-                    // 만약 s의 다음 요소의 인덱스가 전 인덱스가 작으면 false 반환
-                    if (index < lastCharAt) return false
-
-                    lastCharAt = index
-                    matchCount++
-                    return@outer
-                }
+        var sIndex = 0
+        var tIndex = 0
+        while (sIndex < s.length && tIndex < t.length) {
+            if (s[sIndex] == t[tIndex]) {
+                sIndex++
             }
+            tIndex++
         }
-
-        // s 내 문자가 t에서 모두 발견되지 않으면 false 반환
-        return matchCount == s.length
+        return sIndex == s.length
     }
 }
