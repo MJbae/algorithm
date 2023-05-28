1038488868caf24f1bbb83fa81fee228de554eab
배만진 <gentlemmj@gmail.com>
2023-05-28 11:44:12 +0900

    add MaximumAverageSubarrayI_643 with Sliding Window Concept

--- leetCode/src/main/kotlin/leetCode75/sliding_windows/MaximumAverageSubarrayI_643.kt
+++ leetCode/src/main/kotlin/leetCode75/sliding_windows/MaximumAverageSubarrayI_643.kt
@@ -18,7 +18,9 @@ class Solution1a {
         while (nums.size - k >= firstIdx) {
             temp = calMaxArray(nums, firstIdx, k)
 
-            if (result == null){result = temp}
+            if (result == null) {
+                result = temp
+            }
             if (temp > result) result = temp
 
             firstIdx++
@@ -27,12 +29,35 @@ class Solution1a {
         return result
     }
 
-    private fun calMaxArray(nums: IntArray, firstIdx : Int, size: Int): Double {
+    private fun calMaxArray(nums: IntArray, firstIdx: Int, size: Int): Double {
         var sum = 0.0
         (0 until size).forEach {
             sum += nums[firstIdx + it]
         }
 
         return sum / size
+    }
+}
+
+
+// Sliding Window 활용
+class Solution1Best {
+    class Solution {
+        fun findMaxAverage(nums: IntArray, k: Int): Double {
+            val kDouble = k.toDouble()
+            var currAvg = 0.0
+            for (i in 0 until k) {
+                currAvg += nums[i] / kDouble
+            }
+
+            var answer = currAvg
+            for (i in k .. nums.lastIndex) {
+                currAvg -= nums[i - k] / kDouble
+                currAvg += nums[i] / kDouble
+                answer = maxOf(answer, currAvg)
+            }
+
+            return answer
+        }
     }
 }
